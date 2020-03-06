4a60d618fa3685913acb413107a444f3743d8e82
Shohruxpro <shohruxpro@gmail.com>
2020-03-06 20:55:43 +0500

    feat: update index.js with the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+	if (n < 0) 
+	{
+		return Number(String(-n).split('').reverse().join(''))
+	}
+	else
+	{
+  		return Number(String(n).split('').reverse().join(''))
+	}
 }
